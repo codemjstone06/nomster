c2de4a305bf1905c82777ae0622d997aae656aef
Michael Stone <codemjstone06@gmail.com>
2018-04-14 00:58:55 +0000

    API changes

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
   geocoded_by :address
-  after_validation :geocode 
+  after_validation :geocode
 
 
   validates :name, :address, :description, presence: true
