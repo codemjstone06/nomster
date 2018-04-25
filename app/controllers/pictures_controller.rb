d240ace6476304f6a94cc80ad52ddf446cd762df
Michael Stone <codemjstone06@gmail.com>
2018-04-25 03:29:23 +0000

    picture set up cont

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,17 @@
+class PicturesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @place = Place.find(params[:place_id])
+    @place.pictures.create(picture_params.merge(user: current_user))
+    redirect_to place_path(@place)
+  end
+
+   private
+
+  def picture_params
+    params.require(:picture).permit(:caption)
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   resources :places do
     resources :comments, only: :create
   end
+  resources :places do
+    resources :pictures, only: :create
+  end
   
 end
 
